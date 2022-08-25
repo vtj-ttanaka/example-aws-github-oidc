3a7073ca625b41e5994f5cfc73c6c2c35b1b46ae
たなかともあき <86752616+vtj-ttanaka@users.noreply.github.com>
2022-08-25 14:59:21 +0900

    delete description

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,17 +1,14 @@
 variable "repository_name" {
-  description = "ECR repository name"
-  type        = string
-  default     = null
+  type    = string
+  default = null
 }
 
 variable "access_key" {
-  description = "AWS ACCESS_KEY_ID"
-  type        = string
-  default     = null
+  type    = string
+  default = null
 }
 
 variable "secret_key" {
-  description = "AWS SECRET_ACCESS_KEY"
-  type        = string
-  default     = null
+  type    = string
+  default = null
 }
