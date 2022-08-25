453671e5874736f1c4c57d604380268198b71b17
たなかともあき <86752616+vtj-ttanaka@users.noreply.github.com>
2022-08-25 14:23:31 +0900

    added keys to provider

--- infra/variables.tf
+++ infra/variables.tf
@@ -3,3 +3,15 @@ variable "repository_name" {
   type        = string
   default     = null
 }
+
+variable "access_key" {
+  description = "AWS ACCESS_KEY_ID"
+  type        = string
+  default     = null
+}
+
+variable "secret_key" {
+  description = "AWS SECRET_ACCESS_KEY"
+  type        = string
+  default     = null
+}
